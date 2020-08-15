71db44594d11582c5e8a150bbeb9799af24e4de9
biswara890@gmail.com <biswara890@gmail.com>
2020-08-15 16:14:16 +0700

    first

---
+++ database/migrations/2020_08_14_132718_create_customer_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('id_customer');
+            $table->string('nama_customers');
+            $table->text('alamat');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
---
+++ database/migrations/2020_08_14_134327_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id_order');
+            $table->string('nama_order');
+            $table->date('tanggal_order');
+            $table->text('alamat');
+            $table->unsignedBigInteger('id_customer');
+
+            $table->foreign('id_customer')->references('id_customer')->on('customer');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
